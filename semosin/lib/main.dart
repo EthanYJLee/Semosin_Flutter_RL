5a0cb473ac7a8fa68d5c0600a123deed709db0ce
이영진 <lyj72011648@gmail.com>
2023-03-15 14:55:08 +0900

    [수정] Login과 Welcome을 Welcome에 병합 (이메일로그인, 구글로그인, 회원가입, 비밀번호 찾기)

--- semosin/lib/main.dart
+++ semosin/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:semosin/firebase_options.dart';
+import 'package:semosin/view/signup.dart';
 import 'package:semosin/view/welcome.dart';
 
 void main() async {
@@ -37,7 +38,9 @@ class MyApp extends StatelessWidget {
           if (!userSnapshot.hasData) {
             return const Welcome();
           } else {
-            return const ShoesTabBar();
+            // [임시] merge 후 TabBar와 연결할 것
+            // return const ShoesTabBar();
+            return const Signup();
           }
         },
       ),
