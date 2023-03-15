a2ee2f3213ad33530573b56ad700e34399eb903b
coldnoodle <lsyeon98@naver.com>
2023-03-15 10:36:05 +0900

    [추가] welcome page UI

--- semosin/lib/view/welcome.dart
+++ semosin/lib/view/welcome.dart
@@ -1,7 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:semosin/view/login.dart';
+import 'package:semosin/view/signup.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   /// 날짜 :2023.03.13
   /// 작성자 : 권순형
@@ -10,16 +31,125 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(), // 없음
       body: Center(
         child: Column(
-          children: const [
+          children: [
             // 신발 이미지
+            const SizedBox(
+              height: 180,
+            ),
+            SizedBox(
+              height: 150,
+              width: 150,
+              child: Image.asset("images/converse.png"),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
             // 타이틀
+            const Text(
+              "Welcome!!",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+
             // 구글 로그인 버튼 : googleLoginAPI()
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onPressed: () {
+                  signInWithGoogle();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        "images/googlelogo.png",
+                        width: 20,
+                        height: 20,
+                      ),
+                    ),
+                    const Text(
+                      "Continue With Google",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
             // or 텍스트
+            const Text("or"),
+            const SizedBox(
+              height: 25,
+            ),
             // 로그인 화면 이동 버튼 : Login 페이지 이동
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Login(),
+                    ),
+                  );
+                },
+                child: const Text("Sign in with password"),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             // 회원 가입 이동 버튼 : SignUp 페이지 이동
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have account?",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Signup(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
