4593a028fb998bd143f8172d74751c8dddce371a
SMCSMA <msoko89@gmail.com>
2023-03-16 09:47:22 +0900

    [수정] main 주석처리

--- semosin/lib/main.dart
+++ semosin/lib/main.dart
@@ -1,32 +1,32 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:semosin/firebase_options.dart';
-import 'package:semosin/widget/shoestest_widget.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:semosin/firebase_options.dart';
+// import 'package:semosin/widget/shoestest_widget.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  /// 날짜 :2023.03.13
-  /// 작성자 : 권순형
-  /// 만든이 :
-  /// 내용 : firebase auth로 login 했는지 안했는지 체크 하기
-  /// 상세 내용 : login 한적 있으면 Home , 없으면 Welcome
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ImageFromFirestore(),
-    );
-  }
-}
+//   /// 날짜 :2023.03.13
+//   /// 작성자 : 권순형
+//   /// 만든이 :
+//   /// 내용 : firebase auth로 login 했는지 안했는지 체크 하기
+//   /// 상세 내용 : login 한적 있으면 Home , 없으면 Welcome
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: ImageFromFirestore(),
+//     );
+//   }
+// }
