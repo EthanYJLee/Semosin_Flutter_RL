8efaa0dc8d5ba0956d503a1656693d3ecc8d14fb
LeeHosik <tj0bigdata0l@gmail.com>
2023-03-16 10:25:48 +0900

    hosik making shoeslist

--- semosin/lib/view/shoeslist.dart
+++ semosin/lib/view/shoeslist.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:semosin/view_model/shoe_view_model.dart';
+import 'package:semosin/services/firestore_select.dart';
+import 'package:semosin/widget/brand_list.dart';
 
 class ShoesList extends StatefulWidget {
   const ShoesList({super.key});
@@ -16,22 +17,66 @@ class _ShoesListState extends State<ShoesList> {
   /// 만든이 :
   /// 내용 : 검색 , 브랜드 버튼, 정렬 방법 , list view
   /// 상세 내용 : login 한적 있으면 Home , 없으면 Welcome
+  late TextEditingController serach;
+  // late List data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    serach = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
         child: Column(
           children: [
             // app bar 없애서 필요
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
+              height: mq.height * 0.1,
             ),
             // 검색
+            TextField(
+              controller: serach,
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Search Product',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             // 브랜드 버튼
+            Center(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // children: [
+              child: BrandHorizontalList(context),
+              // ],
+            ),
             // drop box
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //
+                  },
+                  child: const Text('임시 가격높은순'),
+                ),
+                const SizedBox(
+                  width: 20,
+                )
+              ],
+            ),
             // Future.builder
             //    - listview.builder 들어갈거 가 widget -> shoe.dart
             //        - 참고사항 : future builder로 부터 받아온 값들을 ShoeViewModel 생성자에 넣어줘야 된다.
+            Column(
+              children: [],
+            ),
           ],
         ),
       ),
@@ -49,7 +94,7 @@ class _ShoesListState extends State<ShoesList> {
   /// 작성자 : 권순형 , 이호식
   /// 만든이 :
   /// 내용 : 신발 데이터 select 위해 firestore와 연결되는 객체 생성 및 함수 호출
-  Future<List<ShoeViewModel>> selectShoesAndLike() async {
-    // firestore_select -> selectShoesAndLike() 함수 호출 해서 데이터 값 받기
-  }
+  // Future<List<ShoeViewModel>> selectShoesAndLike() async {
+  // firestore_select -> selectShoesAndLike() 함수 호출 해서 데이터 값 받기
+  // }
 }
