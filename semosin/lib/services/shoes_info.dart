6e6e0b95de14e4f08edcea0a3a82570a02e3a824
이영진 <lyj72011648@gmail.com>
2023-03-19 01:50:10 +0900

    [추가] ShoeDetail 이미지 출력 추가 (프리뷰 너무 느려서 수정할 예정)

--- semosin/lib/services/shoes_info.dart
+++ semosin/lib/services/shoes_info.dart
@@ -2,8 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:semosin/model/selected_shoe.dart';
+import 'package:semosin/view_model/image_path_view_model.dart';
 
 class ShoesInfo {
+  /// Desc : ShoesList에서 선택한 신발의 상세정보 ShoeDetail에 출력
+  /// Date : 2023.03.18
+  /// Author : youngjin
   Future<SelectedShoe> selectModelNameData(String modelName) async {
     String _modelName = modelName;
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance
@@ -18,5 +22,4 @@ class ShoesInfo {
 
     return _selectedShoe;
   }
-
 }
--- semosin/lib/view/shoedetail.dart
+++ semosin/lib/view/shoedetail.dart
@@ -9,6 +9,7 @@ import 'package:semosin/model/selected_shoe.dart';
 import 'package:semosin/model/shoe.dart';
 import 'package:semosin/services/firebase_firestore.dart';
 import 'package:semosin/services/shoes_info.dart';
+import 'package:semosin/view_model/image_path_view_model.dart';
 import 'package:semosin/view_model/shoe_view_model.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -17,6 +18,8 @@ class ShoeDetail extends StatefulWidget {
 
   // click 시 모델 이름 받아오기
   final String modelName;
+
+  // image path 받아오기
 
   @override
   State<ShoeDetail> createState() => _ShoeDetailState();
@@ -31,11 +34,13 @@ class _ShoeDetailState extends State<ShoeDetail> {
   /// 내용 : shoe detail 정보 보여주기
   late bool isLoading;
   ShoesInfo shoesInfo = ShoesInfo();
+  late ImagePathViewModel imagePathViewModel;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    imagePathViewModel = ImagePathViewModel(imagePath: []);
 
     isLoading = true;
     // 해당 모델의 데이터 받아오는 거 함수 호출
@@ -43,8 +48,15 @@ class _ShoeDetailState extends State<ShoeDetail> {
   }
 
   @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // return isLoading ? loadingTrueWidget() : getDataWidget();
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -56,20 +68,36 @@ class _ShoeDetailState extends State<ShoeDetail> {
               future: shoesInfo.selectModelNameData(widget.modelName),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Container(
-                    height: 300,
-                    width: 200,
-                    child: AlertDialog(
-                      title: Text(snapshot.data!.brand),
-                      content: Text(snapshot.data!.price),
-                    ),
+                  getImageURL(snapshot.data!.images as List<String>);
+                  return Column(
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 300,
+                        child: imagePathViewModel.imagePath.isEmpty
+                            ? const Padding(
+                                padding: EdgeInsets.all(100.0),
+                                child: CircularProgressIndicator(),
+                              )
+                            : ListView.builder(
+                                itemCount: imagePathViewModel.imagePath.length,
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Card(
+                                    child: Image.network(
+                                        imagePathViewModel.imagePath[index]),
+                                  );
+                                },
+                              ),
+                      ),
+                      Text("브랜드: ${snapshot.data!.brand}"),
+                      Text("모델명: ${snapshot.data!.model}"),
+                      Text("가격: ${snapshot.data!.price}"),
+                    ],
                   );
                 } else {
-                  return Container(
-                    height: 100,
-                    width: 100,
-                    child: CircularProgressIndicator(),
-                  );
+                  return Container();
                 }
               },
             )
@@ -102,4 +130,26 @@ class _ShoeDetailState extends State<ShoeDetail> {
   /// 작성자 : 권순형 , 이성연
   /// 만든이 :
   /// 내용 : shoe detail 정보 보여주기
+
+  /// Desc : 신발 이미지 가져오기
+  /// Date : 2023.03.19
+  /// Author : youngjin
+  /// *** 이미지 프리뷰 너무 오래 걸림 (수정 필요할 듯) ***
+  Future<void> getImageURL(List<String> path) async {
+    List<String> pathList = [];
+    for (int i = 0; i < path.length; i++) {
+      var downloadURL = await FirebaseStorage.instance
+          .ref('신발 이미지')
+          .child(path[i].substring(8))
+          .getDownloadURL();
+      // setState(() {
+      pathList.add(downloadURL.toString());
+      // });
+    }
+    if (mounted) {
+      setState(() {
+        imagePathViewModel = ImagePathViewModel(imagePath: pathList);
+      });
+    }
+  }
 }
